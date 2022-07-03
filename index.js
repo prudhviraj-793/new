e94d56a718ef018cac6402e7a7c2f8eda5d806e4
Prudhvi Raj <prudhviraj@gmail.com>
2022-07-03 13:31:39 +0530

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+var itemList = document.querySelector("#items");
+console.log(itemList.firstchild);
+console.log(itemList.nextsibling);
+console.log(itemList.nextelementsibling);
+console.log(itemList.previoussibling);
+console.log(itemList.previouselementsibling);
+
